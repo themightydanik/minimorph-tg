fc156d635725004bab3b93a894ca9c8738ca22ea
themightydanik <94014685+themightydanik@users.noreply.github.com>
2025-07-22 19:23:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ bot.start(async (ctx) => {
 
   const referralParam = `?startapp=${userId}`;
   const startGameLink = `https://t.me/MinimorphBot?startapp=ref${userId}`;
-  const howToPlayLink = 'https://minimorph.space/how-to-play';
+  const howToPlayLink = 'https://minimorph.space/';
   const communityLink = 'https://t.me/minimorph'; // замени на свою ссылку
 
   const keyboard = {

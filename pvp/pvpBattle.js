f4732145b2deb07b39889e8621339c0bf1345fa2
themightydanik <94014685+themightydanik@users.noreply.github.com>
2025-10-28 17:30:19 +0200

    Update pvpBattle.js

--- pvp/pvpBattle.js
+++ pvp/pvpBattle.js
@@ -1,17 +1,13 @@
 // pvp/pvpBattle.js
-import initPvpHandlers from "./pvpHandlers.js";
 import initPvpPayments from "./pvpPayments.js";
 import { initGameLogic } from "./pvpGameLogic.js";
 
 export default function initPvpModule({ bot, db }) {
   console.log("⚔️ Initializing PvP Battle module...");
-  
-  const context = {
-    bot,
-    db,
-  };
 
-  initPvpHandlers(context);
+  const context = { bot, db };
+
+  // Убираем initPvpHandlers — его код теперь в index.js
   initPvpPayments(context);
   initGameLogic(context);
 
